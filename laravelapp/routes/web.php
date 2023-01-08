075db64ad4135e0eda77599539ccfa0f86dfe941
takuma1156 <takumaujiie0906@yahoo.co.jp>
2023-01-08 21:08:11 +0900

    migration

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -26,4 +26,5 @@ Route::get('hello/edit',[HelloController::class,'edit']);
 Route::post('hello/edit',[HelloController::class,'update']);
 Route::get('hello/del',[HelloController::class,'del']);
 Route::post('hello/del',[HelloController::class,'remove']);
+Route::get('hello/show',[HelloController::class,'show']);
 
