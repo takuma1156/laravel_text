a0cac4306e0b118a3a5109e15284891a77602020
takuma1156 <takumaujiie0906@yahoo.co.jp>
2023-01-08 13:58:02 +0900

    aaa

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -10,50 +10,28 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $validator = Validator::make($request->query(),[
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
-        if($validator->fails()){
-            $msg = 'クエリに問題があります';
+        if($request->hasCookie('msg'))
+        {
+            $msg = 'Cookie:' . $request->cookie('msg');
         }else{
-            $msg = 'ID/PASSを受け付けました。フォームを入力ください';
+            $msg = '※クッキーはありません。';
         }
-        return view('hello.index',['msg'=>$msg,]);
+        return view('hello.index',['msg'=>$msg]);
     }
 
     public function post(Request $request)
     {
-        $rules =[
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric',
+        $validate_rule=[
+            'msg' => 'required',
         ];
-
-        $messages=[
-            'name.required' => '名前は必ず入力してください',
-            'mail.email' => 'メールアドレスが必要です',
-            'age.numeric' => '年齢は整数で入力してください',
-            'age.min' => '年齢はゼロ歳以上で記入ください',
-            'age.max' => '年齢は200才以下で記入ください',
-        ];
-
-        $validator = Validator::make($request->all(),$rules,$messages);
-        
-        $validator->sometimes('age', 'min:0',function($input){
-            return is_numeric($input->age);
-        });
-        $validator->sometimes('age', 'max:200',function($input){
-            return is_numeric($input->age);
-        });
-        
-    
-        if($validator->fails()){
-            return redirect('/hello')
-                            ->withErrors($validator)
-                            ->withInput();
-        }
-        return view('hello.index', ['msg'=>'正しく入力されました！']);
+        $this->validate($request, $validate_rule);
+        $msg = $request->msg;
+        $response = response()->view('hello.index',
+            [
+                'msg'=>'「'.$msg . '」をクッキーに保存しました。'
+            ]);
+        $response->cookie('msg',$msg, 100);
+        return $response;
     }
 }
 
--- laravelapp/app/Providers/HelloServiceProvider.php
+++ laravelapp/app/Providers/HelloServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Validator;
+use App\Http\Validators\HelloValidator;
+
 
 class HelloServiceProvider extends ServiceProvider
 {
@@ -24,7 +27,8 @@ class HelloServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer(
-            'hello.index','App\Http\Composers\HelloComposer');
+        Validator::extend('hello', function($attribute, $value, $parameters, $validator){
+            return $value % 2 ==0;
+        });
     }
 }
